872657e88cb7f8124d3b2b6eafcce2b27fba58f7
Jim Baldwin <14864200+jimwbaldwin@users.noreply.github.com>
2024-04-23 22:05:03 +1000

    Add nvim-neotest for python in config folder

---
+++ lua/custom/plugins/nvim-neotest.lua
@@ -0,0 +1,21 @@
+local M = {
+  'nvim-neotest/neotest',
+  dependencies = {
+    'nvim-neotest/nvim-nio',
+    'nvim-neotest/neotest-python',
+    'nvim-lua/plenary.nvim',
+    'antoinemadec/FixCursorHold.nvim',
+    'nvim-treesitter/nvim-treesitter',
+  },
+  config = function()
+    require('neotest').setup {
+      adapters = {
+        require 'neotest-python' {
+          runner = 'pytest',
+        },
+      },
+    }
+  end,
+}
+
+return M
